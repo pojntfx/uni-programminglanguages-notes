0e6b496b5826aed4bb2b4167bc363e91d25bf593
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-11-06 19:21:28 +0100

    refactor: Make the todos app a questions app for the talk

--- docs/static/example/cheatsheet.sh
+++ docs/static/example/cheatsheet.sh
@@ -3,8 +3,8 @@
 rerun 'ruby business.rb'
 rerun 'ruby web.rb'
 
-curl -X POST localhost:4567/api/todos --data-raw '{"title": "asdf", "body": "asdf2"}'
+curl -X POST localhost:4567/api/questions --data-raw '{"title": "asdf", "body": "asdf2"}'
 
-curl -X DELETE localhost:4567/api/todos/81837abd-368c-4108-98fb-4bf8163911c1
+curl -X DELETE localhost:4567/api/questions/81837abd-368c-4108-98fb-4bf8163911c1
 
 npx localtunnel -s pojntfx -p 4567
--- docs/static/example/web.rb
+++ docs/static/example/web.rb
@@ -7,46 +7,46 @@ require './common'
 
 DRb.start_service
 
-todos = DRbObject.new_with_uri DRUBY_URI
+questions = DRbObject.new_with_uri DRUBY_URI
 
 puts "Connected to business on URI #{DRUBY_URI}"
 
-get '/api/todos' do
-  for todo in todos.todos
-    p todo
+get '/api/questions' do
+  for question in questions.questions
+    p question
   end
 
-  json todos.todos
+  json questions.questions
 end
 
-post '/api/todos' do
+post '/api/questions' do
   input = JSON.parse request.body.read
 
-  json todos.add_todo(input['title'], input['body'])
+  json questions.add_question(input['title'], input['body'])
 end
 
-delete '/api/todos/:id' do
-  json todos.delete_todo(params['id'])
+delete '/api/questions/:id' do
+  json questions.delete_question(params['id'])
 end
 
-get '/todos' do
-  @todos = todos.todos
+get '/questions' do
+  @questions = questions.questions
 
-  erb :todos
+  erb :questions
 end
 
-post '/todos' do
-  todos.add_todo(params['title'], params['body'])
+post '/questions' do
+  questions.add_question(params['title'], params['body'])
 
   redirect back
 end
 
-post '/todos/:id' do
-  todos.delete_todo(params['id'])
+post '/questions/:id' do
+  questions.delete_question(params['id'])
 
   redirect back
 end
 
 get '/' do
-  redirect '/todos'
+  redirect '/questions'
 end
